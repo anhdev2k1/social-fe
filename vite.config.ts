16474cad323210f3b16b7532583b211aff6b7a6e
anhdev <info.anh2001@gmail.com>
2023-12-20 23:12:49 +0700

    add rollup typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import requireTransform from 'vite-plugin-require-transform';
+import typescript from '@rollup/plugin-typescript'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [react(), typescript(),
     requireTransform({}),
   ],
 })
