5cba33c29275b299537d7fdca8987f17ad877259
anhdev <info.anh2001@gmail.com>
2023-12-20 23:06:31 +0700

    config vite

--- src/configs/interceptor.ts
+++ src/configs/interceptor.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://social-be-anhdev2k1.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
